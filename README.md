8aad633c668319ef025d79100091f30c37b65836
Magnus Peterson-Munoz <magnuspetersonmunoz@gmail.com>
2022-05-09 07:49:54 -0600

    Add RSI

--- README.md
+++ README.md
@@ -3,15 +3,37 @@
 I am using this tool to attempt to find some common stock market patterns which will either end up in the price going up long-term or going down long-term.  
 
 ## Currently Tracked Trading Tools
-Here is a quick list of what is currently tracked in the FinanceIndicators() class, and what they are:
+
+Here is a quick list of what is currently tracked in the FinanceIndicators() class, and what they are:  
+
+> If you see ^ next to a method parameter, it means that it is not required to provide a value, and probably better off not being changed in most cases.  
+
 - Closing Prices
-    - This extracts what the closing price is for a set time (usually max for most accuracy) and turns it into a list. This makes it easier to extract the actual values we use in the program. Called with fi.extract_cprices()
+    - This extracts what the closing price is for a set time (usually max for most accuracy) and turns it into a list. This makes it easier to extract the actual values we use in the program. 
+    - Called with fi.extract_cprices()
 - Percentage Change
-    - This one is pretty simple. Daily % change = (today's price - yesterday's price) / yesterday's price * 100. If I needed to do anything with the percentage change, I would remove the (* 100) at the end to make it easier. Called with fi.percent_change()
+    - This one is pretty simple. Daily % change = (today's price - yesterday's price) / yesterday's price * 100. If I needed to do anything with the percentage change, I would remove the (* 100) at the end to make it easier. 
+    - Called with fi.percent_change()
 - Simple Moving Average
-    - I figured how to write the code for this one from [geeksforgeeks.com](https://www.geeksforgeeks.com). A simple moving average is the sum of the number of days you want to cover (the average timeframe or window) / the timeframe or window. Called with fi.simple_ma()
+    - I figured how to write the code for this one from [geeksforgeeks.com](https://www.geeksforgeeks.org/how-to-calculate-moving-averages-in-python/). A simple moving average is the sum of the number of days you want to cover (the average timeframe or window) / the timeframe or window. 
+    - Called with fi.simple_ma(prices, period, output_list)
 - Exponential Moving Average
-    - I used the template for the simple_ma to loop through the timeframe, but the formula came from [stockcharts.com](https://school.stockcharts.com/doku.php?id=technical_indicators:moving_averages). The formula for this is really complicated and better explained in the stockcharts link. This is called with fi.exponential_ma()
+    - I used the template for the simple_ma to loop through the timeframe, but the formula came from [stockcharts.com](https://school.stockcharts.com/doku.php?id=technical_indicators:moving_averages). The formula for this is really complicated and better explained in the stockcharts link. 
+    - Called with fi.exponential_ma(prices, period, output_list)
+- MACD
+    - The MACD by itself is calculated by subracting the 12-period EMA from the 26-period EMA. The MACD signal line is the 9-day EMA of the MACD. A MACD histogram is the MACD subtracted from the signal line.
+    - Called with fi.macd_whistogram(macd12^, macd24^, macd9^)
+- RSI
+    - RSI is a very complicated indicator considering what we have done up to this point. RSI works by calculating an average gain / average loss = RS, then 100 - (100/RS) to get RSI. For the first average, it is simply the mean of the first 14 numbers. After that, it is ((last avg. gain * 13 + current gain) / 14) / ((last avg. loss * 13 + current loss) / 14). THen RS and RSI are the same.
+    - Called with fi.rsi(length^)
+
+### Coming up
+
+#### Indicators
+In the future, we plan to release PPO, Money Flow Index, Chalkin Money Flow, Ulcer Index, Chalkin Oscillator  
+
+#### Overlays
+In the future, I plan to release Kaufman's Moving Average, ZigZag, ZigZag (Retrace), and the SMA and EMA Envelopes.
 
 ## Version History  
 > There has not been any released versions yet, but stay tuned as they should come soon!
--- main.py
+++ main.py
@@ -1,75 +1,56 @@
 """
-View license @ LICENCE file
+View license @ ./LICENSE
 
 This is a program that attempts to find common bullish and bearish patterns of stocks.
 
 Currently, it can only find percentages, but this will change in the near future
 """
 
-from tkinter import E
 import yfinance as yf
 from datetime import datetime, timedelta
 import pandas as pd
 import csv
+import numpy as np
+from time import time
 
 thistory = {}
 
 ticker = "AMZN"
-icker = yf.Ticker(ticker)
 exporter = yf.download(ticker, period="max")
-# earnings = icker.calendar
 
 t_df = pd.DataFrame(data=exporter)
 ticker_df = t_df.to_dict()
-# print(exporter)
-keys_tr = ("Open", "High", "Low", "Adj Close", "Volume")
-
-
-def pop_multiple(var, *args):
-    """
-    This is a simple function that can be used to pop multiple items from a list or dictonary
-
-    var -> The variable you want to pop the items or keys
-
-    *args -> The items or keys you want to pop
-    """
-    args = args[0]
-    iter(list(args))
-    for x in args:
-        var.pop(x)
-
-
-pop_multiple(ticker_df, keys_tr)
-# print(ticker_df)
-# print(len(ticker_df["Close"].keys())-1)
-
-# percentage_change = []
-
-# print(percentage_change)
-
 
 d_percent_change = []
-# print(percent_change)
-# eg.append(percent_change)
 
 t_df_keys = ticker_df["Close"].keys()
+t_df_dates = []
 li_close = []
 # Note that lt_df_keys is in a function, but it is a global variable stated with the global keyword.
 
 
 class FinanceIndicators():
     """
-    This class is what stores all the indicators used commonly by investors for figuring out if a security
+    This class is what stores all the indicators used commonly by investors for figuring out if a security is at a good point for investing or not.
     """
 
-    def __init__(self, simple_ma1_length=None, simple_ma2_length=None, ema1_ol=None, ema2_ol=None, ema1_length=None, ema2_length=None):
+    def __init__(self, sma1_ol=None, sma2_ol=None, simple_ma1_length=None, simple_ma2_length=None, ema1_ol=None, ema2_ol=None, ema1_length=None, ema2_length=None, rsi_ol=None,ppo_ol=None):
         self.simple_ma1_length = simple_ma1_length
         self.simple_ma2_length = simple_ma2_length
+        self.sma1_ol = sma1_ol
+        self.sma2_ol = sma2_ol
         self.ema1_ol = ema1_ol
         self.ema2_ol = ema2_ol
         self.ema1_length = ema1_length
         self.ema2_length = ema2_length
+        self.rsi_ol = rsi_ol
+        self.ppo_ol = ppo_ol
         # self.simple_ma3_length = simple_ma3_length
+
+    def extract_dates(self):
+        t_df_values = []
+        for v in ticker_df["Close"].keys():
+            t_df_dates.append(v)
 
     def extract_values(self):
         """
@@ -92,7 +73,7 @@ class FinanceIndicators():
             finally:
                 pass
 
-        print(li_close[slice(1, 51, 1)])
+        # print(li_close[slice(1, 51, 1)])
 
     def percent_change(self):
         """
@@ -108,34 +89,27 @@ class FinanceIndicators():
             d_percent_change.append(percent_change)
         # print(d_percent_change)
 
-    def simple_ma(self):
+    def simple_ma(self, prices, length, output):
         """
         Credit for helping me figure this out goes to https://www.geeksforgeeks.org/how-to-calculate-moving-averages-in-python/
 
         This class method finds the Simple Moving Average for a stock over a certain period of time. Currently only supports 2 different values
 
-        There are no parameters you have to enter in for the function, but rather the self.simple_ma_length value held in the __init__ function
+        Parameters: prices - The list of prices you want to find the SMA of
+
+        length: The length / window of time you want to find for the SMA.
+
+        output_list: The list you want the SMA values to be extracted to.
         """
-        averages = []
         i = 0
 
-        while i < len(li_close) - self.simple_ma1_length + 1:
-            timeframe = li_close[i:i+self.simple_ma1_length]
+        while i < len(prices) - length + 1:
+            timeframe = prices[i:i+length]
 
-            tf_average = sum(timeframe) / self.simple_ma1_length
-            averages.append(tf_average)
+            tf_average = sum(timeframe) / length
+            output.append(tf_average)
             i += 1
-        # print(averages)
-        averages2 = []
-        j = 0
-
-        while j < len(li_close) - self.simple_ma2_length + 1:
-            timeframe2 = li_close[i:i+self.simple_ma2_length]
-
-            tf_average2 = sum(timeframe2) / self.simple_ma2_length
-            averages2.append(tf_average2)
-            j += 1
-        # print(averages)
+        print(output)
 
     def exponential_ma(self, prices, length, output_list):
         """
@@ -147,16 +121,9 @@ class FinanceIndicators():
 
         output_list: The list you want the EMA values to be extracted to.
 
-        Calculation for a 20-day exponential moving average is:
-
-        First day = SMA
-
-        Multiplier = (2 / (20 + 1)) 
-
-        EMA = (Close - EMA(previous day)) * multiplier + EMA (previous day)
+        EMA: https://school.stockcharts.com/doku.php?id=technical_indicators:moving_averages
         """
-        multiplier = (2 / (self.ema1_length+1))
-        # print(multiplier)
+        multiplier = (2 / (length+1))
         i = 0
         e_fv = sum(prices[0:length]) / length
         output_list.append(e_fv)
@@ -166,30 +133,135 @@ class FinanceIndicators():
                 window[-1] - output_list[-1]) * multiplier + output_list[-1]
             output_list.append(e_window_average)
             i += 1
-        print(output_list)
 
-    def rsi(self):
-        pass
+    def macd_whistogram(self, macd_short=12, macd_long=26, macd_sl=9):
+        """
+        The MACD is a calculation of the 12-period EMA - the 26-period EMA. A more advanced calculation (MACD histogram) uses a signal line, which is the 9-period EMA of the MACD itself, which is subtracted by the original MACD value.
+        """
+        global m_histogram
+        global macd_9ema
+        global macd
+        m_histogram = []
+        macd_short = 12
+        macd_long = 26
+        macd_sl = 9
+        macd12 = []
+        macd26 = []
+        macd = []
+        macd_9ema = []
+        self.exponential_ma(li_close, macd_short, macd12)
+        self.exponential_ma(li_close, macd_long, macd26)
+        # print(len(macd12))
+        # print(len(macd26))
+        for x in range(0, len(macd26), 1):
+            macd_calc = macd12[x+14]-macd26[x]
+            macd.append(macd_calc)
+        # print(macd)
+        self.exponential_ma(macd, macd_sl, macd_9ema)
+        for y in range(0, len(macd_9ema), 1):
+            hist_calc = macd[y+7] - macd_9ema[y]
+            m_histogram.append(hist_calc)
+        # print(m_histogram)
 
-    def macd(self):
-        self.ema1_length = 12
-        self.ema2_length = 26
-        print(self.ema1_length)
+    def rsi(self, output_list, length=14):
+        """
+        Calculates the RSI based on the closing price of a security. 
+        Full credit for helping me figure out how to implement this into my program goes to alpharitims.com (link inside README.md)
+
+        Parameters
+        ----------
+        output_list:  `list`
+        The list you want to extract the RSI values to.
+
+        length: `int`
+        The length to calculate RSI. Default is 14, and a change is not recommended.
+
+        """
+        df = pd.DataFrame(li_close)
+        # Get positive and negative price differences
+        df[1] = df.diff(1)
+        df[2] = df[1].clip(lower=0).round(2)
+        df[3] = df[1].clip(upper=0).abs().round(2)
+
+        # Get Average positive and negative price difference
+        # Get WMS averages
+        df[4] = df[2].rolling(window=length, min_periods=length).mean()[
+            :length+1]
+        df[5] = df[3].rolling(window=length, min_periods=length).mean()[
+            :length+1]
+        # Average Gains
+        for i, row in enumerate(df[4].iloc[length+1:]):
+            df[4].iloc[i + length + 1] = (df[4].iloc[i + length]
+                                          * (length - 1) + df[2].iloc[i + length + 1]) / length
+
+        # Average Losses
+        for i, row in enumerate(df[5].iloc[length+1:]):
+            df[5].iloc[i + length + 1] = (df[5].iloc[i + length]
+                                          * (length - 1) + df[3].iloc[i + length + 1]) / length
+
+        # View initial results
+        df[6] = df[4] / df[5]
+        df[7] = 100 - (100 / (1 + df[6]))
+        output_list.append(list(df[7]))
+    def ppo(self,output_list):
+        output_list = []
+        macd26 = []
+        self.exponential_ma(li_close, 26, macd26)
+        for x in range(0,len(macd),1):
+            output_list.append((macd[x] / macd26[x]) * 100)
 
 
+# Defining fi and the variables inside
 fi = FinanceIndicators()
-# fi.__init__()
+fi.sma1_ol = []
+fi.sma2_ol = []
 fi.simple_ma1_length = 50
 fi.simple_ma2_length = 200
-fi.ema1_ol = e1_moving_averages = []
-fi.ema2_ol = e2_moving_averages = []
+fi.ema1_ol = []
+# fi.ema2_ol = []
 fi.ema1_length = 20
-fi.ema2_length = 50
+fi.rsi_ol = []
+# fi.ema2_length = 50
 # fi.simple_ma3_length = 0
-fi.extract_values()
-fi.extract_cprices()
-fi.percent_change()
-fi.simple_ma()
-fi.exponential_ma(li_close, fi.ema1_length, fi.ema1_ol)
-# fi.exponential_ma(li_close, fi.ema2_length, )
-# print(len(t_df_keys))
+# eg = [7,7,9,674,6,78,8,6,7,4,7,8,6]
+
+
+def nan_generator(amount, output_list):
+    """ Takes the np.zeros() array and adds it to a non-numpy list
+    This very simple function takes an arroy of np.zeros() and puts it into the first values of a non-numpy list.
+
+    Parameters
+    ----------
+    amount : int
+        The amount of zeros you need to use.
+    output_list : list
+        The list you want to add the zeros to.
+    """
+    zeros_var = []
+    for x in range(0, amount, 1):
+        output_list.insert(0, float("NaN"))
+    # print(zeros_var)
+
+
+if __name__ == "__main__":
+    fi.extract_dates()
+    fi.extract_values()
+    fi.extract_cprices()
+    fi.percent_change()
+    fi.simple_ma(li_close, fi.simple_ma1_length, fi.sma1_ol)
+    fi.simple_ma(li_close, fi.simple_ma2_length, fi.sma2_ol)
+    fi.macd_whistogram()
+    fi.rsi(fi.rsi_ol)
+    nan_generator(1, d_percent_change)
+    nan_generator(fi.simple_ma1_length-1, fi.sma1_ol)
+    nan_generator(fi.simple_ma2_length-1, fi.sma2_ol)
+    nan_generator(24, macd)
+    nan_generator(31, macd_9ema)
+    nan_generator(31, m_histogram)
+    # This runs the zip() method and exports the values to a CSV file. Only enabled in stable packages, otherwise commented out.
+    # DO NOT DELETE THE BELOW LINES
+    all_indicators = zip(t_df_dates, li_close, d_percent_change, fi.sma1_ol,
+                         fi.sma2_ol, macd, macd_9ema, m_histogram, fi.rsi_ol[0])
+    ai_df = pd.DataFrame(all_indicators, columns=[
+        "Timestamp", "Close", "Daily Percent Change", "50-Day MA", "200-Day MA", "MACD", "MACD Signal Line", "MACD Histogram", "RSI"])
+    # ai_df.to_csv(f'CSVinfo/{ticker} {time()}.csv', index=False)
